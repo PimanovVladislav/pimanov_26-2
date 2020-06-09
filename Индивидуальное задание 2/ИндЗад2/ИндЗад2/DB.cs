e46e1e1688c6a304a6cd334e2bf7b522c6d7002a
Vlad <pimanovlad@gmail.com>
2020-06-09 19:37:17 +0300

    \\\

---
+++ Индивидуальное задание 2/ИндЗад2/ИндЗад2/DB.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ИндЗад2
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=127.0.0.1;user=root;database=birjha_truda");
+
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
--- Индивидуальное задание 2/ИндЗад2/ИндЗад2/FormCompany.cs
+++ Индивидуальное задание 2/ИндЗад2/ИндЗад2/FormCompany.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ИндЗад2
 {
@@ -15,6 +16,41 @@ namespace ИндЗад2
         public FormCompany()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string connectString = "Data Source=.\\SQLEXPRESS;Initial Catalog=LSTU_Schedule_autumn20172018;" +
+                "Integrated Security=true;";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+
+            string query = "SELECT * FROM Faculty ORDER BY fac_id";
+
+            SqlCommand command = new SqlCommand(query, myConnection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<string[]> data = new List<string[]>();
+
+            while (reader.Read())
+            {
+                data.Add(new string[3]);
+
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][1] = reader[1].ToString();
+                data[data.Count - 1][2] = reader[2].ToString();
+            }
+
+            reader.Close();
+
+            myConnection.Close();
+
+            foreach (string[] s in data)
+                dataGridView1.Rows.Add(s);
         }
     }
 }
